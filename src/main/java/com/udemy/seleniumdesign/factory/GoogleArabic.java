8e8f22311e20ce875b9040174e8fc782e7013e26
silver3456 <avseliverov@gmail.com>
2021-03-05 19:32:26 -0800

    add GoogleFactory

--- src/main/java/com/udemy/seleniumdesign/factory/GoogleArabic.java
+++ src/main/java/com/udemy/seleniumdesign/factory/GoogleArabic.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class GoogleArabic extends GoogleEnglish{
+ class GoogleArabic extends GoogleEnglish{
 
     @FindBy(css = "#SIvCob a")
     private WebElement language;
--- src/main/java/com/udemy/seleniumdesign/factory/GoogleEnglish.java
+++ src/main/java/com/udemy/seleniumdesign/factory/GoogleEnglish.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-public class GoogleEnglish extends GooglePage {
+ class GoogleEnglish extends GooglePage {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
